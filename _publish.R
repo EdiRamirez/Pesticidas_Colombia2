116a47fbad9e7118cc4f2cafb8fc01db74e3d88b
EdiRamírez <edissonramirez7@gmail.com>
2024-04-21 11:05:51 -0500

    Ajustes

--- _publish.R
+++ _publish.R
@@ -4,7 +4,7 @@ unlink('_book', recursive = TRUE)
 
 x = readLines('index.Rmd')
 i = 1
-s = paste0('title: "PREDICCIÓN PARA EL PRECIO DE IMPORTACIÓN DE LOS PESTICIDAS (', c('Bootstrap', 'Tufte'), ' Style)"')
+s = paste0('title: "A Minimal Book Example (', c('Bootstrap', 'Tufte'), ' Style)"')
 for (fmt in c('html_book', 'tufte_html_book')) {
   unlink('_book', recursive = TRUE)
   file.copy('index.Rmd', '_index.Rmd')
